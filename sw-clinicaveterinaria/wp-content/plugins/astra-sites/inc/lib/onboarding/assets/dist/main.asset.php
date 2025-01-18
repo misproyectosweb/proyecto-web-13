50159b95f05a4287e53237da029f51255d1e87ef
Paulo Cesar Gomez Carmiol <paulocgomez70@gmail.com>
2025-01-18 10:28:55 -0600

    Eliminando carpeta de archivos

--- sw-clinicaveterinaria/wp-content/plugins/astra-sites/inc/lib/onboarding/assets/dist/main.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => 'a2f7b28ff7d0eb74d419');
